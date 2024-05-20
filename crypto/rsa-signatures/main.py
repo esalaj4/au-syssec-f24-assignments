7f120423f096775c52e8a3b038caf71dd6b6c957
Ena Salaj <86615856+esalaj4@users.noreply.github.com>
2024-05-20 23:59:27 +0200

    Update main.py

--- crypto/rsa-signatures/main.py
+++ crypto/rsa-signatures/main.py
@@ -8,6 +8,12 @@ import secrets
 
 app = Flask(__name__)
 
+def ceil_div(a, b):
+    return -(-a // b)
+
+def _and_byte(a, b):
+    return bytes([a & b])
+
 def encode_emsa_pss(message: bytes, em_bits: int) -> bytes:
     if len(message) > ((2**64)-1):
         raise ValueError('Too long')
@@ -17,7 +23,8 @@ def encode_emsa_pss(message: bytes, em_bits: int) -> bytes:
     m_hash = sha_256.digest()
     h_len = len(m_hash)
     s_len = 32
-    em_len = rsa_pss.integer_ceil(em_bits, 8)
+    em_len = ceil_div(em_bits, 8)
+
 
     if em_len < (s_len + h_len + 2):
         raise ValueError('Encoding error')
@@ -34,18 +41,21 @@ def encode_emsa_pss(message: bytes, em_bits: int) -> bytes:
 
     DB = PS + b'\x01' + salt
 
-    db_mask = rsa_pss.mgf256(H, em_len - h_len - 1)
+    db_mask = rsa_pss.mask_generation_function(H, em_len - h_len - 1)
 
-    db_mask = rsa_pss.string_xor(DB, db_mask)
+
+    db_mask = rsa_pss.xor_bytes(DB, rsa_pss.mask_generation_function(H, em_len - h_len - 1))
+
 
     octets, bits = (8 * em_len - em_bits) // 8, (8 * em_len - em_bits) % 8
     db_mask = (b'\x00' * octets) + db_mask[octets:]
-    new_byte = bytes([db_mask[octets] & 255 >> bits])
+    new_byte = rsa_pss._and_byte(db_mask[octets], 255 >> bits)
     db_mask = db_mask[:octets] + new_byte + db_mask[octets + 1:]
 
     EM = db_mask + H + b'\xbc'
 
     return EM
+
 
 def sign_rsassa_pss(message: bytes) -> bytes:
     N = rsa_key['_n']
@@ -54,9 +64,9 @@ def sign_rsassa_pss(message: bytes) -> bytes:
 
     EM = encode_emsa_pss(message, (mod_bits - 1))
 
-    m = rsa_pss.os2ip(EM)
-    s = rsa_pss.rsasp1((d, N), m)
-    S = rsa_pss.i2osp(s, 128 * 3)
+    m = rsa_pss.bytes_to_int(EM)
+    s = rsa_pss.rsa_private_sign((d, N), m)
+    S = rsa_pss.int_to_bytes(s, 128 * 3)
 
     return S
 
@@ -70,6 +80,7 @@ def verify_emsa_pss(message: bytes, em: bytes, em_bits: int) -> bool:
     h_len = len(m_hash)
     s_len = 32
     em_len = rsa_pss.integer_ceil(em_bits, 8)
+
 
     if em_len < (s_len + h_len + 2):
         return False
@@ -85,12 +96,12 @@ def verify_emsa_pss(message: bytes, em: bytes, em_bits: int) -> bool:
         if not rsa_pss._byte_eq(c, b'\x00'):
             return False
 
-    db_mask = rsa_pss.mgf256(h, em_len - h_len - 1)
+    db_mask = rsa_pss.mask_generation_function(h, em_len - h_len - 1)
 
-    db = rsa_pss.string_xor(masked_db, db_mask)
+    db_mask = rsa_pss.xor_bytes(masked_db, db_mask)
 
-    new_byte = rsa_pss._and_byte(db[octets], 255 >> bits)
-    db = (b'\x00' * octets) + new_byte + db[octets + 1:]
+    new_byte = rsa_pss._and_byte(db_mask[octets], 255 >> bits)
+    db = (b'\x00' * octets) + new_byte + db_mask[octets + 1:]
 
     for c in db[:em_len - h_len - s_len - 2]:
         if not rsa_pss._byte_eq(c, b'\x00'):
@@ -113,19 +124,17 @@ def verify_emsa_pss(message: bytes, em: bytes, em_bits: int) -> bool:
 
 def verify_rsassa_pss(message: bytes, signature: bytes) -> bool:
     n = rsa_key['_n']
-    d = rsa_key['_d']
     e = rsa_key['_e']
-
     mod_bits = 3072
     em_bits = mod_bits - 1
     em_len = rsa_pss.integer_ceil(em_bits, 8)
 
-    if(len(signature) != 128 * 3):
+    if len(signature) != 128 * 3:
         return False
 
-    s = rsa_pss.os2ip(signature)
+    s = rsa_pss.bytes_to_int(signature)
     m = rsa_pss.rsavp1((n, e), s)
-    EM = rsa_pss.i2osp(m, em_len)
+    EM = rsa_pss.int_to_bytes(m, em_len)
 
     verified = verify_emsa_pss(message, EM, em_bits)
 
@@ -141,7 +150,7 @@ def pk():
     e = int(rsa_key['_e'])
     return {'N': N, 'e': e}
 
-'''
+
 @app.route('/grade/')
 def grade():
     if 'grade' in request.cookies:
@@ -158,7 +167,7 @@ def grade():
             return response
     else:
         g = secrets.choice(['-3', '00', '02', '4', '7', '10'])
-        msg = f'You get a only get a {g} in System Security. I am very disappointed by you.'.encode()
+        msg = f'You get only get a {g} in System Security. I am very disappointed by you.'.encode()
         signature = sign_rsassa_pss(msg)
         j = json.dumps({'msg': msg.hex(), 'signature': signature.hex()})
         response = make_response('<p>Here is your grade, and take a cookie!</p>')
@@ -173,5 +182,4 @@ def quote():
         signature = bytes.fromhex(j['signature'])
     except Exception as e:
         print(e)
-        return '<p>Grading is not yet done, come back next year.</p'
-'''
+        return '<p>Grading is not yet done, come back next year.</p>'
